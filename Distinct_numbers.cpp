89d19856d8d0c023cc8ee54fc7a50558ac05feed
Sadia Afrin <116829514+meerror6d@users.noreply.github.com>
2023-10-20 20:01:21 +0600

    Distinct_numbers.cpp

--- Distinct_numbers.cpp
+++ Distinct_numbers.cpp
@@ -11,25 +11,6 @@
 
 using namespace std;
 
-//ll binary(ll a,ll b)
-//{
-//    ll mid=(a+b)/2;
-//
-//    if((n*mid*mid+(m*mid)-c)==0)cout<<mid/2<<endl;
-//    else if((n*mid*mid+(m*mid)>c))binary(mid+1,b);
-//    else binary(a,mid-1);
-//}
-
-ll summ(ll a)
-{
-    ll b=a,sum=0;
-    for(ll i=1; i<=19; i++)
-    {
-        sum+=b%10;
-        b/=10;
-    }
-    return sum;
-}
 int main()
 {
  set<ll>s;
